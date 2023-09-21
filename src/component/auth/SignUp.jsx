5ac50ddc6f7e8ea67496952c58b7e43effe7555d
wadi <zabir4897@gmail.com>
2023-09-21 22:13:13 +0600

    create sign up

---
+++ src/component/auth/SignUp.jsx
@@ -0,0 +1,17 @@
+import './static/signup.scss'
+import sign-up from '../auth/' ''
+const SignUp = () => {
+  return (
+    <div className='signup-container'>
+      <div className='signup-left'>
+        <h5>Create an account</h5>
+        <img src={signup-img} alt="signup-img" />
+      </div>
+      <div className='signup-right'>
+
+      </div>
+    </div>
+  )
+}
+
+export default SignUp
